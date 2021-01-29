d6c50815a7247b08660f1dec55fcef381ee51294
Glenn Bach <glenn.w.bach@gmail.com>
2021-01-28 18:56:20 -0800

    menu updates

--- wildewidgets/__init__.py
+++ wildewidgets/__init__.py
@@ -3,15 +3,22 @@ __version__ = "0.1.13"
 from .wildewidgets import (
     AltairChart,
     BarChart, 
-    DataTable,
-    DataTableFilter,
     BasicMenu,
     DoughnutChart,
     Histogram,
     HorizontalBarChart, 
     HorizontalHistogram,
     HorizontalStackedBarChart,
+    LightMenu,
     PieChart,
     StackedBarChart, 
     WildewidgetDispatch,
 )
+
+try:
+    from .wildewidgets import (
+        DataTable,
+        DataTableFilter,
+    )
+except ImportError:
+    pass
