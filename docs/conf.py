7698c3a7af51744f6bca5b35d53fd7d4ad1a9a6c
Glenn Bach <glenn@caltech.edu>
2021-10-05 11:20:24 -0700

    Bump version: 0.5.7 → 0.5.8

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Glenn Bach'
 author = 'Glenn Bach'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.7'
+release = '0.5.8'
 
 
 # -- General configuration ---------------------------------------------------
