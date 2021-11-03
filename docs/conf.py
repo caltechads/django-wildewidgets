1baaf0c79312638068286a485818cdeed9c0749d
Glenn Bach <glenn@caltech.edu>
2021-11-03 16:42:22 -0700

    Bump version: 0.7.3 → 0.8.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Glenn Bach'
 author = 'Glenn Bach'
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.3'
+release = '0.8.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-wildewidgets',
-    version="0.7.3",
+    version="0.8.0",
     description='django-wildewidgets is a Django library designed to help you make charts, graphs, tables, and UI widgets quickly and easily with libraries like Chartjs, Altair, and Datatables.',
     long_description=open('README.md', 'rt').read(),
     long_description_content_type="text/markdown",
