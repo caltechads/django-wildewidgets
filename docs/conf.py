c0e97c263ce8a462e930e16ee3bf23cc92cea0c0
Glenn Bach <glenn@caltech.edu>
2022-01-10 15:58:34 -0800

    Bump version: 0.12.5 → 0.12.6

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Glenn Bach'
 author = 'Glenn Bach'
 
 # The full version, including alpha/beta/rc tags
-release = '0.12.5'
+release = '0.12.6'
 
 
 # -- General configuration ---------------------------------------------------
