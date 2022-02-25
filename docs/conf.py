39332d6c9f401f374171155bcc32d50e6c284789
Chris Malek <cmalek@caltech.edu>
2022-02-25 15:27:58 -0800

    Bump version: 0.13.19 → 0.13.20

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Glenn Bach'
 author = 'Glenn Bach'
 
 # The full version, including alpha/beta/rc tags
-release = '0.13.19'
+release = '0.13.20'
 
 
 # -- General configuration ---------------------------------------------------
