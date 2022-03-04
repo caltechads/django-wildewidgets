0bde364f26728fd5a86acbda3caeb49041b1862d
Glenn Bach <glenn@caltech.edu>
2022-03-04 15:55:54 -0800

    modal fixes

--- wildewidgets/widgets/modals.py
+++ wildewidgets/widgets/modals.py
@@ -11,6 +11,11 @@ class ModalWidget(TemplateWidget):
     modal_id = None
     modal_title = None
 
+    def __init__(self, **kwargs):
+        self.modal_id = kwargs.pop('modal_id', self.modal_id)
+        self.modal_title = kwargs.pop('modal_title', self.modal_title)
+        super().__init__(**kwargs)
+
     def get_context_data(self, **kwargs):
         kwargs = super().get_context_data(**kwargs)
         kwargs['modal_id'] = self.modal_id
@@ -22,6 +27,11 @@ class CrispyFormModalWidget(ModalWidget):
     template_name = 'wildewidgets/crispy_form_modal.html'
     form_class = None
     form = None
+
+    def __init__(self, **kwargs):
+        self.form_class = kwargs.pop('form_class', self.form_class)
+        self.form = kwargs.pop('form', self.form)
+        super().__init__(**kwargs)
 
     def get_context_data(self, **kwargs):
         kwargs = super().get_context_data(**kwargs)
