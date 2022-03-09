810b4224114b2760ac0d8c43e5e879b87b259635
Glenn Bach <glenn@caltech.edu>
2022-03-09 14:03:29 -0800

    Bump version: 0.13.27 → 0.13.28

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, California Institute of Technology'
 author = 'Glenn Bach'
 
 # The full version, including alpha/beta/rc tags
-release = '0.13.27'
+release = '0.13.28'
 
 
 # -- General configuration ---------------------------------------------------
--- wildewidgets/__init__.py
+++ wildewidgets/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.13.27"
+__version__ = "0.13.28"
 
 from .widgets import *  # noqa: F403,F401
 from .menus import *  # noqa: F403,F401
