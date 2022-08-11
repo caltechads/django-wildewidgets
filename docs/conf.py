48ebd663313869748718b60c787cc43587566a34
Glenn Bach <glenn@caltech.edu>
2022-08-11 11:47:37 -0700

    trying again with favicon

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,6 @@ html_context = {
 #
 html_theme_options = {
     'collapse_navigation': True,
-    'display_version': True,
     'navigation_depth': 3,
     "show_prev_next": False,
     "logo": {
@@ -87,10 +86,10 @@ html_theme_options = {
             "type": "fontawesome",
         }
    ],
-   "html_favicon": "_static/favicon.ico",
 }
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_logo = "_static/wildewidgets.png"
+html_favicon = "_static/favicon.ico"
