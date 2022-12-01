b95125614792cac6cd4cc918c3c87e40c8bf96a4
Glenn Bach <glenn@caltech.edu>
2022-12-01 11:56:37 -0800

    Bump version: 0.13.53 → 0.13.54

--- demo/setup.py
+++ demo/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="demo",
-    version="0.13.53",
+    version="0.13.54",
     description="",
     author="Caltech IMSS ADS",
     author_email="imss-ads-staff@caltech.edu",
--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, California Institute of Technology'
 author = 'Glenn Bach'
 
 # The full version, including alpha/beta/rc tags
-release = '0.13.53'
+release = '0.13.54'
 
 
 # -- General configuration ---------------------------------------------------
