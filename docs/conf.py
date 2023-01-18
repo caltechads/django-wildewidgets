4e761851a46eb5b1ba84d9ca030266c081841193
Chris Malek <cmalek@caltech.edu>
2023-01-18 15:57:54 -0800

    Bump version: 0.14.1 → 0.14.2

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ from typing import List, Dict, Tuple, Optional
 
 
 # The full version, including alpha/beta/rc tags
-release = '0.14.1'
+release = '0.14.2'
 
 
 # -- General configuration ---------------------------------------------------
