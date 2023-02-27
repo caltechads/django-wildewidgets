4fb2f954e261cca906266ca28c02c2ee3d35d16f
Chris Malek <cmalek@caltech.edu>
2023-02-27 14:34:04 -0800

    Bump version: 0.15.4 → 0.15.5

--- demo/setup.py
+++ demo/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="demo",
-    version="0.15.4",
+    version="0.15.5",
     description="",
     author="Caltech IMSS ADS",
     author_email="imss-ads-staff@caltech.edu",
--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ from typing import List, Dict, Tuple, Optional
 
 
 # The full version, including alpha/beta/rc tags
-release = '0.15.4'
+release = '0.15.5'
 
 
 # -- General configuration ---------------------------------------------------
