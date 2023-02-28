c164feec900376436895fb39c816a9be04fea369
Glenn Bach <glenn@caltech.edu>
2023-02-28 14:43:29 -0800

    Bump version: 0.15.5 → 0.16.0

--- demo/setup.py
+++ demo/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="demo",
-    version="0.15.5",
+    version="0.16.0",
     description="",
     author="Caltech IMSS ADS",
     author_email="imss-ads-staff@caltech.edu",
--- wildewidgets/__init__.py
+++ wildewidgets/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.15.5"
+__version__ = "0.16.0"
 
 from .widgets import *  # noqa: F403,F401
 from .menus import *  # noqa: F403,F401
