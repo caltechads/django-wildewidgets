15138616f2b07320db33b2169f84e230d91bfbe0
Glenn Bach <glenn@caltech.edu>
2023-03-16 13:32:54 -0700

    Bump version: 0.16.1 → 0.16.2

--- demo/setup.py
+++ demo/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="demo",
-    version="0.16.1",
+    version="0.16.2",
     description="",
     author="Caltech IMSS ADS",
     author_email="imss-ads-staff@caltech.edu",
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='django-wildewidgets',
-    version="0.16.1",
+    version="0.16.2",
     packages=find_packages(exclude=['bin']),
     include_package_data=True,
     description='django-wildewidgets is a Django design library providing several tools for '
