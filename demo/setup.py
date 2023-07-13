479b0971e7c0bb6b65bf26f8c76b3276ee76628d
Glenn Bach <glenn@caltech.edu>
2023-07-13 15:58:56 -0700

    Bump version: 0.16.10 → 0.16.11

--- demo/setup.py
+++ demo/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="demo",
-    version="0.16.10",
+    version="0.16.11",
     description="",
     author="Caltech IMSS ADS",
     author_email="imss-ads-staff@caltech.edu",
--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ from typing import List, Dict, Tuple, Optional
 
 
 # The full version, including alpha/beta/rc tags
-release = '0.16.10'
+release = '0.16.11'
 
 
 # -- General configuration ---------------------------------------------------
