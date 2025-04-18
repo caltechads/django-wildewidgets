2cd9a55613001fef45db274f20b5a58404c23991
Chris Malek <cmalek@caltech.edu>
2025-04-18 16:29:49 -0700

    Bump version: 1.0.1 → 1.0.2

--- demo/setup.py
+++ demo/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="demo",
-    version="1.0.1",
+    version="1.0.2",
     description="",
     author="Caltech IMSS ADS",
     author_email="imss-ads-staff@caltech.edu",
