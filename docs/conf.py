8ffe1d5d5d0e05f5e66f101b5af94ccb2868e1cf
Chris Malek <cmalek@caltech.edu>
2025-04-28 15:13:44 -0700

    Bump version: 1.1.0 → 1.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ from typing import Dict, Tuple, Optional  # noqa: E402
 
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.0"
+release = "1.1.1"
 
 
 # -- General configuration ---------------------------------------------------
