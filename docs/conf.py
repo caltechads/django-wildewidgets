6a35dd23563de2a18e1e57779dabce20ffab68bc
Chris Malek <cmalek@caltech.edu>
2025-04-28 15:05:05 -0700

    Bump version: 1.0.4 → 1.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ from typing import Dict, Tuple, Optional  # noqa: E402
 
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.4"
+release = "1.1.0"
 
 
 # -- General configuration ---------------------------------------------------
--- wildewidgets/__init__.py
+++ wildewidgets/__init__.py
@@ -1,7 +1,7 @@
 <<<<<<< HEAD
 __version__ = "1.0.3"
 =======
-__version__ = "1.0.4"
+__version__ = "1.1.0"
 >>>>>>> 9c048d89de74711d5fa544178912efe7b87ce810
 
 from .widgets import *  # noqa: F403,F401
