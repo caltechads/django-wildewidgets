a31dd35761ee277a22c79ffc2cefa31de5bc3919
Glenn Bach <glenn@caltech.edu>
2025-04-29 10:09:42 -0700

    Bump version: 1.1.1 → 1.1.2

--- demo/setup.py
+++ demo/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="demo",
-    version="1.1.1",
+    version="1.1.2",
     description="",
     author="Caltech IMSS ADS",
     author_email="imss-ads-staff@caltech.edu",
--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ from typing import Dict, Tuple, Optional  # noqa: E402
 
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.1"
+release = "1.1.2"
 
 
 # -- General configuration ---------------------------------------------------
